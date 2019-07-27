621fbf63fccfdb873739e4a348d2d847052374b1
Backslash <lanceadrian13@gmail.com>
2019-07-27 17:01:02 -0400

    added functionality

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# liri-node-app
+## Liri 
+is a command line node app that takes in parameters and returns data. The user has the option of using several commands along with with specific parameters associated with the commands.
+
+## Motivation
+The Liri project is inspired by Apple's Siri. That being said Liri exists to do similar things utilizing several apis to get information that the user wants.
+
+## Technologies 
+
+<b>Built with node.js</b>
+- [React](https://reactjs.org/)
+<b>Is a platform built on Chrome's JavaScript runtime for easily building fast and scalable network applications. I used it to utilize its packages to get users request<b>
+
+## Features
+It utilizes efficent code utilizing a switch case to account for the variety of request a user can make.
+This includes requesting information about an artist, where are some locations he is playing in and information about the songs he makes. Additionally it also has the functionality to search for a movies information such as IMDB ratings, actors and the movie's plot.
+
+
+## Instructions
+
+1.Open your terminal
+
+2.run "node liri.js spotify-this-song + (name of any song you like)"
+
+3.This should return information such as the artist of the song, the album it's from and a preview.
+
+4.If you run "node liri.js" you'll find a list of commands that Liri can accomplish.
+
+## License
+[Lance]()
--- liri.js
+++ liri.js
@@ -1,28 +1,131 @@
 require("dotenv").config();
 
+var axios = require("axios");
+var fs = require("fs");
+var moment = require('moment'); 
+moment().format()
+
 var keys = require("./keys.js");
+
+var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
 
 var term = process.argv[2]; 
 var search = process.argv[3];
-
+var divider = "\n--------------------------------------------------------------------"
 UserInputs(term, search);
 
 function UserInputs (term, search){
     switch (term) {
     case 'concert-this':
-        showConcertInfo(search);
+        concertInfo(search);
         break;
     case 'spotify-this-song':
-        showSongInfo(search);
+        songInfo(search);
         break;
     case 'movie-this':
-        showMovieInfo(search);
+        movieInfo(search);
         break;
     case 'do-what-it-says':
-        showSomeInfo();
+        whatItSays();
         break;
     default: 
-        console.log("Invalid Option. Please type any of the following options: \nconcert-this \nspotify-this-song \nmovie-this \ndo-what-it-says")
+        console.log("Invalid Request. \nPlease type one of the following options: \nconcert-this \nspotify-this-song \nmovie-this \ndo-what-it-says")
     }
+}
+
+function concertInfo(artist){
+    
+var URL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+var label= "\n*******Concert Information for " + artist+ "*******";
+
+axios.get(URL).then(function(response) {
+    console.log(label);
+    fs.appendFileSync("log.txt","\n*******Concert Information for " + artist+ "*******")
+    for (var i = 0; i < response.data.length; i++) {
+      
+        var datetime = response.data[i].datetime; 
+        var dateArr = datetime.split('T'); 
+        
+       
+        var concertResults = 
+            divider +
+                "\nVenue Name: " + response.data[i].venue.name + 
+                "\nVenue Location: " + response.data[i].venue.city +
+                "\nDate of the Event: " + moment(dateArr[0], "YYYY-MM-DD").format("MM-DD-YYYY"); //dateArr[0] should be the date separated from the time
+      
+        console.log(concertResults);
+        fs.appendFileSync("log.txt",concertResults+"\n");
+    }
+
+})
+.catch(function (error) {
+    console.log(error);
+});
+};
+
+function songInfo(song){
+    var label= "\n*******Song Information for " + song+ "*******";
+    fs.appendFileSync("log.txt", "\n*******Information for " + song+ "*******")
+    if(!song){
+        song = "The Sign";
+    }
+    spotify.search({ type: 'track', query: song })
+    .then(function(data) {
+        for (var i = 0; i < data.tracks.items[0].artists.length; i++) {
+            var spotifyResults =
+            "\n"+divider+
+                    "\nArtist(s): " + data.tracks.items[0].artists[i].name + 
+                    "\nSong Name: " + data.tracks.items[0].name +
+                    "\nPreview Link: " + data.tracks.items[0].preview_url +
+                    "\nAlbum Name: " + data.tracks.items[0].album.name + "\n"
+                    ;
+                    
+            console.log(spotifyResults);
+            fs.appendFileSync("log.txt", spotifyResults +"\n");
+        }
+    })
+    .catch(function(err) {
+        console.log(err);
+       
+
+   
+      });
+}
+
+function movieInfo(movie){
+var URL = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=b3c0b435";
+var label= "\n*******Movie Information for " + movie + "*******";
+fs.appendFileSync("log.txt","\n*******Movie Information for " + movie + "*******")
+axios.get(URL).then(function(response) {
+    
+    var jsonData = (response.data)
+
+    var movieData = [
+        divider+
+        "Title: " + jsonData.Title,
+        "Year: " + jsonData.Year,
+        "imdb Rating: " + jsonData.Ratings[0].Value,
+        "Rotten Tomatoes Rating: " + jsonData.Ratings[1].Value,
+        "Country: " + jsonData.Country,
+        "Language: " + jsonData.Language,
+        "Pilot: " +jsonData.Plot,
+        "Actors: "+ jsonData.Actors,
+
+
+    ].join("\n\n");
+    console.log(movieData);
+    fs.appendFileSync("log.txt", movieData +"\n");
+    })
+}
+
+function whatItSays(){
+	fs.readFile('random.txt', 'utf8', function(error, data){
+		if (error){ 
+			return console.log(error);
+		}
+        var dataArr = data.split(',');
+        UserInputs(dataArr[0], dataArr[1]);
+        fs.appendFileSync("log.txt", data);
+	});
 }
